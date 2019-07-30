c3b2497364dc5f2becaac985b48053e0a0ac1ae0
AllveGit <createpubpriv@gmail.com>
2019-07-30 19:59:18 +0900

    피스톤 추가

---
+++ Assets/Script/Intro/Piston.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston : MonoBehaviour
+{
+    public Vector2 TargetPos;
+    public float fSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 Dir = TargetPos - (Vector2)transform.position;
+        Dir.Normalize();
+
+        (Vector2)transform.position += Dir * Time.deltaTime * fSpeed;
+    }
+}
