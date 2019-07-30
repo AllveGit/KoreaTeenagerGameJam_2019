2a841fa1632765fc5b11d81158776b3b0db10851
AllveGit <createpubpriv@gmail.com>
2019-07-30 21:15:49 +0900

    AntiVirus 추가

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject m_Player;
+    public float CamSpeed;
+    public float smoothTime = 0.01f;
+
+    void Awake()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("PlayerHead");
+    }
+
+    void LateUpdate()
+    {
+        Vector3 Dir = m_Player.transform.position - this.transform.position;
+        float fDist = Dir.magnitude;
+
+
+        Vector3 curDir = new Vector3(0,0,0);
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, m_Player.transform.position, ref curDir, smoothTime);
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -10);
+    }
+}
---
+++ Assets/Script/Intro/AntiVirus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiVirus : MonoBehaviour
+{
+    float fSpeed = 0.1f;
+    float fAccel = 3;
+    private GameObject m_Player;
+
+    void Awake()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("PlayerHead");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 vDir = m_Player.transform.position - this.transform.position;
+
+        float fDist = vDir.magnitude;
+
+        if(fDist < 5f)
+        {
+            fSpeed += fAccel * Time.fixedDeltaTime;
+            fAccel += 1 * Time.fixedDeltaTime;
+
+            transform.position += vDir * Time.fixedDeltaTime * fSpeed; 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+
+            Destroy(this.gameObject);
+        }
+    }
+}
