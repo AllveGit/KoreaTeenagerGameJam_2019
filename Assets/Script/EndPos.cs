910fcabf69589496e231260286da0e77658e1f76
AllveGit <createpubpriv@gmail.com>
2019-07-31 02:45:05 +0900

    아 모ㅑㄹ라 ㅈ나많이추가함

--- Assets/Script/EndPos.cs
+++ Assets/Script/EndPos.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndPos : MonoBehaviour
 {
+    bool bChangeScene = false;
+    public int SceneNum = 0;
+    private float ChangeTime = 3f;
+
     void Start()
     {
         
@@ -11,16 +16,26 @@ public class EndPos : MonoBehaviour
 
     void Update()
     {
-        
+        if (bChangeScene)
+        {
+            ChangeTime -= Time.deltaTime;
+
+            if (ChangeTime <= 0)
+            {
+                ChangeTime = 0;
+                SceneManager.LoadScene(SceneNum);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == 8)
+        if (collision.gameObject.layer == 8)
         {
             GameObject.Find("FadeManager").GetComponent<Fade>().Dir = 1;
             StartCoroutine(GameObject.Find("FadeManager").GetComponent<Fade>().Fading());
             GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().timeScale = 0f;
+            bChangeScene = true;
         }
     }
 }
