a655428699e6e41f064f2fa41239d1c07e63c520
AllveGit <createpubpriv@gmail.com>
2019-07-30 22:37:55 +0900

    안티바이러스 사이즈조절

---
+++ Assets/Script/Effect/Fade/Fade.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour
+{
+    public int Dir;
+    FadeManager m_Fade;
+
+    void Start()
+    {
+        m_Fade = FindObjectOfType<FadeManager>();
+        StartCoroutine(Fading());
+    }
+
+    public IEnumerator Fading()
+    {
+        float FadeTime = m_Fade.BeginFade(Dir);
+        yield return new WaitForSeconds(FadeTime);
+    }
+}
--- Assets/Script/Intro/AntiVirus.cs
+++ Assets/Script/Intro/AntiVirus.cs
@@ -4,28 +4,61 @@ using UnityEngine;
 
 public class AntiVirus : MonoBehaviour
 {
-    float fSpeed = 0.1f;
-    float fAccel = 3;
+    bool bScaleUp = false;
+    float fScale = 0.6f;
+    float fSpeed = 5f;
+    float Angle = 0f;
+    Vector3 Dir = new Vector3(0,0,0);
     private GameObject m_Player;
 
     void Awake()
     {
         m_Player = GameObject.FindGameObjectWithTag("PlayerHead");
+        while(Dir.y == 0 || Dir.x == 0)
+            Dir = new Vector3(Random.Range(-1, 2), Random.Range(-1, 2), 0);
+
+        Angle = Random.Range(0f, 360f);
+        transform.localEulerAngles = new Vector3(0, 0, Angle);
     }
 
     void FixedUpdate()
     {
+        if(!bScaleUp)
+        {
+            fScale -= Time.fixedDeltaTime;
+
+            if (fScale < 0.5f)
+            {
+                bScaleUp = true;
+                fScale = 0.5f;
+            }
+        }
+
+        else
+        {
+            fScale += Time.fixedDeltaTime;
+
+            if (fScale > 0.6f)
+            {
+                bScaleUp = false;
+                fScale = 0.6f;
+            }
+        }
+
+        transform.localScale = new Vector3(fScale, fScale, 1);
+
         Vector3 vDir = m_Player.transform.position - this.transform.position;
 
         float fDist = vDir.magnitude;
 
-        if(fDist < 5f)
-        {
-            fSpeed += fAccel * Time.fixedDeltaTime;
-            fAccel += 1 * Time.fixedDeltaTime;
-
-            transform.position += vDir * Time.fixedDeltaTime * fSpeed; 
+        if (fDist < 2f)
+        { 
+            
+            transform.position += vDir * Time.fixedDeltaTime * fSpeed;
         }
+
+        else
+            transform.position += Dir * Time.fixedDeltaTime * 2;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -33,6 +66,11 @@ public class AntiVirus : MonoBehaviour
         if(collision.gameObject.layer == 8 || collision.gameObject.tag == "Enemy")
         {
             Destroy(this.gameObject);
+        }
+
+        else if(collision.gameObject.tag == "Wall")
+        {
+            Dir = -Dir;
         }
     }
 }
