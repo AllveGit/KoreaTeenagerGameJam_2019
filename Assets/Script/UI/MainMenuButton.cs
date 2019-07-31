4ff3ab40206932b8cb98151e0f9b3f2c1010b1d2
AllveGit <createpubpriv@gmail.com>
2019-07-31 17:33:20 +0900

    메인메뉴 UI 추가

---
+++ Assets/Script/UI/MainMenuButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public Sprite IdleSprite;
+    public Sprite OverSprite;
+    bool bScaleup = false;
+    float fScale = 1f;
+
+    public void OnEnter()
+    {
+        GetComponent<Image>().sprite = OverSprite;
+        bScaleup = true;
+    }
+
+    public void OnExit()
+    {
+        GetComponent<Image>().sprite = IdleSprite;
+        bScaleup = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if(bScaleup)
+        {
+            fScale += Time.deltaTime;
+
+            if (fScale > 1.2f)
+                fScale = 1.2f;
+        }
+
+        else
+        {
+            fScale -= Time.deltaTime;
+
+            if (fScale < 1f)
+                fScale = 1f;
+        }
+
+        GetComponent<Image>().transform.localScale = new Vector3(fScale, fScale, 1f);
+    }
+}
---
+++ Assets/Script/UI/Title.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    bool bUp = false;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(!bUp)
+        {
+            RectTransform MyTransform = GetComponent<RectTransform>();
+
+            MyTransform.localPosition -= new Vector3(0,1,0) * Time.deltaTime * 30;
+
+            if (MyTransform.localPosition.y <= -150)
+            {
+                MyTransform.localPosition = new Vector3(MyTransform.localPosition.x, -150, 0);
+                bUp = true;
+            }
+        }
+
+        else
+        {
+            RectTransform MyTransform = GetComponent<RectTransform>();
+
+            MyTransform.position += new Vector3(0, 1, 0) * Time.deltaTime * 30;
+
+            if (MyTransform.position.y >= -80)
+            {
+                MyTransform.position = new Vector3(MyTransform.localPosition.x ,-80, 0);
+                bUp = false;
+            }
+        }
+    }
+}
