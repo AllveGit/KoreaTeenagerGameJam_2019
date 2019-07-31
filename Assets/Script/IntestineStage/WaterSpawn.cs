d6dc1b4c815e2fb0919775eaa90f7a391dd2771a
AllveGit <createpubpriv@gmail.com>
2019-07-31 21:10:27 +0900

    댐잇

---
+++ Assets/Script/IntestineStage/WaterSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSpawn : MonoBehaviour
+{
+    public GameObject ObjectSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 40; i++)
+        {
+            Instantiate(ObjectSpawn, transform.position, Quaternion.identity, null);
+        }
+
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(30f);
+
+            for (int i = 0; i < 40; i++)
+            {
+                Instantiate(ObjectSpawn, transform.position, Quaternion.identity, null);
+            }
+        }
+    }
+}
---
+++ Assets/Script/StageFadeText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageFadeText : MonoBehaviour
+{
+    public bool bEnabled = false;
+    public bool bAlphaEnd = false;
+    public float iAlpha = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (bAlphaEnd)
+        {
+            iAlpha -= Time.fixedDeltaTime;
+
+            if (iAlpha < 0)
+            {
+                iAlpha = 0;
+                Destroy(this.gameObject);
+            }
+        }
+
+        else if(bEnabled)
+        {
+            iAlpha += Time.fixedDeltaTime;
+
+            if(iAlpha > 1)
+            {
+                iAlpha = 1;
+                bAlphaEnd = true;
+            }
+        }
+
+        GetComponent<Image>().color = new Color(1, 1, 1, iAlpha);
+    }
+}
